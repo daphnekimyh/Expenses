c712e233b58ed745ee4c3a8d5ba8ad6470b6c0d9
Daphne Kim <101921685+daphnekimyh@users.noreply.github.com>
2023-07-19 13:12:37 +0900

    Update Chart.js

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -4,7 +4,16 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
-  return <div className="chart"></div>;
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
+
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoints) => (
+        <ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={totalMaximum} label={dataPoint.label} />
+      ))}
+    </div>
+  );
 };
 
 export default Chart;
